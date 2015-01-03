c956912489b558892226f14cda64c2d280d7a895
David Khavari <dkhavari@gmail.com>
2015-01-03 15:55:01 -0800

    Wrote the first blog post, still haven't hooked up the DB backend to store it yet.

--- views.py
+++ views.py
@@ -6,3 +6,13 @@ from flask import render_template, request, redirect, url_for
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+# Blog post display page.
+@app.route("/blog.html")
+def blog():
+	# TODO:
+	# --------------------------------------------------
+	# Get the name of the blog post from the args,
+	# retrieve from the database, then render the content.
+	# --------------------------------------------------
+	return render_template("blog.html")
